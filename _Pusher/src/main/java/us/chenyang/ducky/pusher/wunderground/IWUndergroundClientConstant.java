21c8e1179aca941b125eeb1d4953f8a5f2f85ac2
Jerry Yang <zhenyoung@gmail.com>
2017-08-20 14:55:17 -0700

    Add netatmo and WUnderground API. Parameterize pusher

---
+++ _Pusher/src/main/java/us/chenyang/ducky/pusher/wunderground/IWUndergroundClientConstant.java
@@ -0,0 +1,5 @@
+package us.chenyang.ducky.pusher.wunderground;
+
+public interface IWUndergroundClientConstant {
+    public static final String WUnderground_UPLOAD = "https://weatherstation.wunderground.com/weatherstation/updateweatherstation.php";
+}
